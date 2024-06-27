c9da4e9072f66cb7ec86a3f77511890ae8ec2dfd
felixNyalenda <felixasejunior@gmail.com>
2024-06-27 20:19:56 +0300

    profile right menu bar build

--- src/components/UserInfoCard.tsx
+++ src/components/UserInfoCard.tsx
@@ -1,7 +1,108 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function UserInfoCard({ userId }: { userId: string }) {
-  return <div className="card text-sm">UserInfoCard</div>;
+  return (
+    <div className="card text-sm flex flex-col gap-4">
+      <div className="flex-between font-medium">
+        <span className="text-gray-500">User Information</span>
+        <Link href="/profile/ase" className="text-blue-500 text-xs">
+          See all
+        </Link>
+      </div>
+
+      <div className="flex flex-col gap-4 text-gray-500">
+        <div className="flex items-center gap-2">
+          <span className="text-black text-base">Cody McCormick</span>
+          <span className="text-sm">@cormick</span>
+        </div>
+
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam libero
+          neque quos repellat praesentium pariatur.
+        </p>
+
+        <div className="flex items-center gap-2">
+          <Image
+            src="/map.png"
+            alt="map"
+            width={16}
+            height={16}
+            className="object-contain size-4"
+          />
+          <span className="">
+            Living in <b>Nairobi</b>
+          </span>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Image
+            src="/school.png"
+            alt="school"
+            width={16}
+            height={16}
+            className="object-contain size-4"
+          />
+          <span className="">
+            Went to <b>Highrise High School</b>
+          </span>
+        </div>
+
+        <div className="flex items-cen ter gap-2">
+          <Image
+            src="/work.png"
+            alt="work"
+            width={16}
+            height={16}
+            className="object-contain size-4"
+          />
+          <span className="">
+            Works at <b>Microsoft Inc</b>
+          </span>
+        </div>
+
+        <div className="flex-between">
+          <div className="flex items-center gap-1">
+            <Image
+              src="/link.png"
+              alt="link"
+              width={16}
+              height={16}
+              className="object-contain size-4"
+            />
+
+            <Link
+              href={"http://asedesign.netlify.app/"}
+              className="text-blue-500 font-medium"
+            >
+              aseDesign
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <Image
+              src="/date.png"
+              alt="date"
+              width={16}
+              height={16}
+              className="object-contain size-4"
+            />
+
+            <span>Joined January, 2014</span>
+          </div>
+        </div>
+
+        <button className="bg-blue-500 text-white text-sm rounded-md p-2">
+          Follow
+        </button>
+
+        <span className="text-red-400 self-end text-xs cursor-pointer">
+          Block User
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default UserInfoCard;
--- src/components/UserMediaCard.tsx
+++ src/components/UserMediaCard.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function UserMediaCard({ userId }: { userId: string }) {
-  return <div className="card text-sm">UserMediaCard</div>;
+  return (
+    <div className="card text-sm flex flex-col gap-4">
+      <div className="flex-between font-medium">
+        <span className="text-gray-500">User Media</span>
+        <Link href="/profile/ase" className="text-blue-500 text-xs">
+          See all
+        </Link>
+      </div>
+
+      <div className="flex gap-4 justify-between flex-wrap">
+        {Array.from({ length: 8 }).map((_, i) => (
+          <div className="relative w-1/5 h-24" key={i}>
+            <Image
+              src="https://images.pexels.com/photos/25019973/pexels-photo-25019973/free-photo-of-close-up-portrait-of-a-woman-in-a-black-top.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+              alt=""
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default UserMediaCard;
